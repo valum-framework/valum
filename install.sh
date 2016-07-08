5e0d7a4d1d6529e0703ecf0d02f7d4aa4997449c
Guillaume Poirier-Morency <guillaumepoiriermorency@gmail.com>
2016-07-08 14:44:05 -0400

    build: Quote variables in 'install.sh' and remove implicit '--target-glib'

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-mkdir -p ${DESTDIR}${MESON_INSTALL_PREFIX}/share/vala/vapi
+mkdir -p "${DESTDIR}${MESON_INSTALL_PREFIX}/share/vala/vapi"
 
-install -m 0644                                            \
-    ${MESON_BUILD_ROOT}/src/vsgi/vsgi.vapi                 \
-    ${MESON_BUILD_ROOT}/src/valum/valum.vapi               \
-    ${DESTDIR}${MESON_INSTALL_PREFIX}/share/vala/vapi
+install -m 0644                                \
+    "${MESON_BUILD_ROOT}/src/vsgi/vsgi.vapi"   \
+    "${MESON_BUILD_ROOT}/src/valum/valum.vapi" \
+    "${DESTDIR}${MESON_INSTALL_PREFIX}/share/vala/vapi"
