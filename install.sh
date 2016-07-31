ad6602d0ac4209937b26d199ee146ddc1d17fc8f
Guillaume Poirier-Morency <guillaumepoiriermorency@gmail.com>
2016-07-30 23:12:37 -0400

    build: Add version to build files (fix #184)
    Meson 0.33 is required to generate specific shared library names as it
    is now based on the target name rather than the first file.

--- install.sh
+++ install.sh
@@ -3,6 +3,6 @@
 mkdir -p "${DESTDIR}${MESON_INSTALL_PREFIX}/share/vala/vapi"
 
 install -m 0644                                \
-    "${MESON_BUILD_ROOT}/src/vsgi/vsgi.vapi"   \
-    "${MESON_BUILD_ROOT}/src/valum/valum.vapi" \
+    "${MESON_BUILD_ROOT}/src/vsgi/vsgi-0.3.vapi"   \
+    "${MESON_BUILD_ROOT}/src/valum/valum-0.3.vapi" \
     "${DESTDIR}${MESON_INSTALL_PREFIX}/share/vala/vapi"
