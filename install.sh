86fd1598b71aa92f8a763e64a00e3cba14f07c73
Guillaume Poirier-Morency <guillaumepoiriermorency@gmail.com>
2016-08-01 09:53:44 -0400

    build: Remove 'install.sh' and install VAPI with 'install_data'

--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-mkdir -p "${DESTDIR}${MESON_INSTALL_PREFIX}/share/vala/vapi"
-
-install -m 0644                                \
-    "${MESON_BUILD_ROOT}/src/vsgi/vsgi-0.3.vapi"   \
-    "${MESON_BUILD_ROOT}/src/valum/valum-0.3.vapi" \
-    "${DESTDIR}${MESON_INSTALL_PREFIX}/share/vala/vapi"
