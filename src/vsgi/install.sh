f3be6a92865ea53099992e0940ed0aafd31e1378
Guillaume Poirier-Morency <guillaumepoiriermorency@gmail.com>
2017-05-03 09:12:32 -0400

    build: Use Meson 0.40 with 'install_dir' extension
    Remove install scripts completely since we can now install C headers and
    VAPIs from the target itself.
    
    Use 'install_data' for installing deps files.

--- src/vsgi/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-mkdir -p "${DESTDIR}/${MESON_INSTALL_PREFIX}/include/vsgi-0.4"
-mkdir -p "${DESTDIR}/${MESON_INSTALL_PREFIX}/share/vala/vapi"
-
-install "${MESON_BUILD_ROOT}/src/vsgi/vsgi.h" "${MESON_INSTALL_DESTDIR_PREFIX}/include/vsgi-0.4"
-install "${MESON_BUILD_ROOT}/src/vsgi/vsgi-0.4.vapi" "${MESON_INSTALL_DESTDIR_PREFIX}/share/vala/vapi"
-install "${MESON_SOURCE_ROOT}/src/vsgi/vsgi-0.4.deps" "${MESON_INSTALL_DESTDIR_PREFIX}/share/vala/vapi"
